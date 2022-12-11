dc8808957fe22eae5f9b6d47ba47db560f29ff1c
mirrorcall <mirrorcall96@gmail.com>
2022-12-11 15:06:32 +0300

    change logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import decair from "./decair.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Would i survive titanic?</p>
+        by
+        <img
+          style={{ width: "20%", height: "20%" }}
+          src={decair}
+          className="App-logo"
+          alt="logo"
+        />
       </header>
     </div>
   );
